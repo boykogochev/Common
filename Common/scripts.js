c63d8cadaa3a694b8e98edc2e04d50d2111ffdc3
boyko_gpchev <gochev.boyko@gmail.com>
2016-08-20 19:06:03 +0300

    20 august

--- Common/scripts.js
+++ Common/scripts.js
@@ -5,16 +5,20 @@ $.fn.gallery = function (columnsPerRow) {
   var $galleryList = $gallery.find('.gallery-container');
   var $selected = $gallery.children('.selected');
   var $img = $gallery.find('img');
-
+ var $selectedImg = $selected.children('img');
 
   $img.on('click', function(){
     var clickedImg = $(this);
     var src = clickedImg.attr('src');
     console.log(src);
-    $img.attr('src', src);
+    $selectedImg.attr('src', src);
     console.log($selected.attr('src'));
 
     $selected.show();
+  });
+
+  $selected.on('click', function(){
+    $selected.hide();
   });
   $selected.hide();
 
