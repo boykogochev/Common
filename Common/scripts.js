c63cc01c6afc81c4c6339270142cdbb3dbc5fb61
boyko_gpchev <gochev.boyko@gmail.com>
2016-08-22 22:58:54 +0300

    22 August

--- Common/scripts.js
+++ Common/scripts.js
@@ -1,27 +1,34 @@
-$.fn.gallery = function (columnsPerRow) {
-  columnsPerRow = columnsPerRow || 4;
+$.fn.gallery = function(columnsPerRow) {
+    columnsPerRow = columnsPerRow || 4;
 
-  var $gallery = $(this);
-  var $galleryList = $gallery.find('.gallery-container');
-  var $selected = $gallery.children('.selected');
-  var $img = $gallery.find('img');
- var $selectedImg = $selected.children('img');
+    var $gallery = $(this);
+    var $galleryList = $gallery.find('.gallery-container');
+    var $selected = $gallery.children('.selected');
+    var $img = $gallery.find('.image-in-gallery');
+    var $selectedImg = $selected.children('.selected-image');
+    var $leftArrow = $selected.children('.arrow');
 
-  $img.on('click', function(){
-    var clickedImg = $(this);
-    var src = clickedImg.attr('src');
-    console.log(src);
-    $selectedImg.attr('src', src);
-    console.log($selected.attr('src'));
+    $img.on('click', function() {
+        var clickedImg = $(this);
+        var src = clickedImg.attr('src');
+        console.log(src);
+        $selectedImg.attr('src', src);
 
-    $selected.show();
-  });
 
-  $selected.on('click', function(){
+        $selected.show();
+
+    });
+
+    $leftArrow.on('mouseover', function(){
+      console.log('dhaj');
+      $leftArrow.show();
+    });
+
+    $selected.on('click', function() {
+        $selected.hide();
+    });
     $selected.hide();
-  });
-  $selected.hide();
-
+    $leftArrow.hide();
 
 
 
