7191f9e8784c5cb6db792ee028a835c90e06642a
boyko_gpchev <gochev.boyko@gmail.com>
2016-09-13 20:29:39 +0300

    13 Sept

--- Common/shots1/mainscript.js
+++ Common/shots1/mainscript.js
@@ -6,7 +6,38 @@ $.fn.buttons = function(columnsPerRow) {
   console.log($icnoElemen);
   /*var $galleryList = $gallery.find('.gallery-container');*/
 
-  $icnoElemen.on('click', function(){
-    console.log('item is clicked');
+//  $icnoElemen.on('click', function(){
+//    console.log('item is clicked');
+//  })
+
+  var $facebook = $('#facebook');
+  var $youtube = $('#youtube');
+  var $twitter = $('#twitter');
+  var $instagram = $('#instagram');
+  var $google = $('#google');
+
+  $facebook.on('click', function(){
+    console.log('facebook is clicked');
+    window.location.href = 'https://www.facebook.com/theshotsclub/?ref=ts&fref=ts';
+  })
+
+  $youtube.on('click', function(){
+    console.log('youtube is clicked');
+    /*TODO create The Shots Chanel*/
+  })
+
+  $twitter.on('click', function(){
+    console.log('twitter is clicked');
+    /*TODO create The Shots Twitter*/
+  })
+
+  $instagram.on('click', function(){
+    console.log('instagram is clicked');
+    /*TODO create The Shots Instagram*/
+  })
+
+  $google.on('click', function(){
+    console.log('google is clicked');
+    /*TODO create The Shots googple page*/
   })
 }
